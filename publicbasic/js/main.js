c7fe992bb816fe4bbc5718c8d797be9351ed7030
barkley <barkley@users-MacBook-Pro.local>
2022-03-20 11:34:00 -0500

    when new message gets pushed, adjust scroll height of chat messages container to show new msgs at the bottom of chat

--- publicbasic/js/main.js
+++ publicbasic/js/main.js
@@ -1,10 +1,15 @@
 const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
 
 const socket = io();
 
 // since this is the client side, we'll catch the socket.emit here from server.js
+// Message from server
 socket.on("message", (message) => {
-	console.log(message);
+	outputMessage(message);
+
+	// Scroll down
+	chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Message Submit event
@@ -14,9 +19,25 @@ chatForm.addEventListener("submit", (e) => {
 	//  getting the thing tht makes the event, check its elements, get the one with id 'msg'
 	const msg = e.target.elements.msg.value;
 
-
 	// emitting a message to the server from client side
 	socket.emit("chatMessage", msg);
+	e.target.elements.msg.value = "";
 });
 
-// Socketio-NodeChat
+// Reset input area for next message
+// function resetInput(){
+//   document.getElementById("elementid").value = "";
+// }
+
+//  Output message to DOM
+function outputMessage(message) {
+	const div = document.createElement("div");
+	div.classList.add("message");
+	div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+<p class="text">${message}</p>`;
+	// console.log(div);
+
+	let messagesContainer = document.querySelector(".chat-messages");
+	messagesContainer.appendChild(div);
+	// resetInput();
+}
